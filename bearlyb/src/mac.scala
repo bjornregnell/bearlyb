41f3eef15f30b865d1042daa43c40d11fa525415
lego-eden <hampus.eden@gmail.com>
2025-02-17 21:27:16 +0100

    switch check for start on first thread

--- bearlyb/src/mac.scala
+++ bearlyb/src/mac.scala
@@ -15,26 +15,19 @@ def restartJVM(args: String*): Unit = boundary:
   if !osName.startsWith("Mac") && !osName.startsWith("Darwin") then break()
 
   val mxBean = ManagementFactory.getRuntimeMXBean()
-  
-  // get current jvm process pid
-  val pid = mxBean.getName().split("@")(0)
-  // get environment variable on whether XstartOnFirstThread is enabled
-  val env = sys.env.lift("JAVA_STARTED_ON_FIRST_THREAD_" + pid)
-  
-  // if environment variable is "1" then XstartOnFirstThread is enabled
-  env match
-    case Some("1") =>
-    case _ => break()
 
+  val inputArgs = mxBean.getInputArguments().asScala.toSeq
+
+  if inputArgs.exists(_ == "-XstartOnFirstThread") then break()
 
   // restart jvm with -XstartOnFirstThread
   println(Console.YELLOW_B + Console.BLACK + "Starting a new JVM with -XstartOnNewThread! Hello mac user :)")
   val classpath = sys.props("java.class.path")
-  val mainClass = sys.props("JAVA_MAIN_CLASS_" + pid)
+  val mainClass = sys.props("JAVA_MAIN_CLASS_" + mxBean.getPid())
   val jvmPath = os.Path(sys.props("java.home"))/"bin"/"java"
   val jvmArgs = Seq(
     "-XstartOnFirstThread",
-  ) ++ mxBean.getInputArguments().asScala ++ Seq(
+  ) ++ inputArgs ++ Seq(
     "-cp",
     classpath,
     mainClass
