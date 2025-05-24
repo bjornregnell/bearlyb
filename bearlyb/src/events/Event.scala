4049835decf6b46d833e08c5247f7018db2fedc0
Hampus Edén <hampus.eden@gmail.com>
2025-05-24 05:25:08 +0200

    make timestamp an explicit part of each sybtype of Event

--- bearlyb/src/events/Event.scala
+++ bearlyb/src/events/Event.scala
@@ -14,103 +14,104 @@ import bearlyb.touch
 import bearlyb.pen.{PenID, PenInputFlags, PenAxis}
 import bearlyb.camera.CameraID
 
-trait Event(val timestamp: Deadline = Deadline.now)
+trait Event:
+  def timestamp: Deadline
 object Event:
   /** User-requested quit */
-  object Quit extends Event
-  object Terminating extends Event
-  object LowMemory extends Event
-  object WillEnterBackground extends Event
-  object DidEnterBackground extends Event
-  object WillEnterForeground extends Event
-  object DidEnterForeground extends Event
-  object LocaleChanged extends Event
-  object ThemeChanged extends Event
+  case class Quit(timestamp: Deadline) extends Event
+  case class Terminating(timestamp: Deadline) extends Event
+  case class LowMemory(timestamp: Deadline) extends Event
+  case class WillEnterBackground(timestamp: Deadline) extends Event
+  case class DidEnterBackground(timestamp: Deadline) extends Event
+  case class WillEnterForeground(timestamp: Deadline) extends Event
+  case class DidEnterForeground(timestamp: Deadline) extends Event
+  case class LocaleChanged(timestamp: Deadline) extends Event
+  case class ThemeChanged(timestamp: Deadline) extends Event
   
   enum Display extends Event:
     def id: DisplayID
 
-    case Orientation(id: DisplayID)
-    case Added(id: DisplayID)
-    case Removed(id: DisplayID)
-    case Moved(id: DisplayID)
-    case DesktopModeChanged(id: DisplayID)
-    case CurrentModeChanged(id: DisplayID)
-    case ContentScaleChanged(id: DisplayID)
+    case Orientation(timestamp: Deadline, id: DisplayID)
+    case Added(timestamp: Deadline, id: DisplayID)
+    case Removed(timestamp: Deadline, id: DisplayID)
+    case Moved(timestamp: Deadline, id: DisplayID)
+    case DesktopModeChanged(timestamp: Deadline, id: DisplayID)
+    case CurrentModeChanged(timestamp: Deadline, id: DisplayID)
+    case ContentScaleChanged(timestamp: Deadline, id: DisplayID)
 
   enum Window extends Event:
     def id: WindowID
 
     /** Window has been shown */
-    case Shown(id: WindowID)
+    case Shown(timestamp: Deadline, id: WindowID)
 
     /** Window has been hidden */
-    case Hidden(id: WindowID)
+    case Hidden(timestamp: Deadline, id: WindowID)
 
     /** Window has been exposed and should be redrawn, and can be redrawn directly
       * from event watchers for this event
       */
-    case Exposed(id: WindowID)
+    case Exposed(timestamp: Deadline, id: WindowID)
 
     /** Window has been moved to x, y */
-    case Moved(id: WindowID, x: Int, y: Int)
+    case Moved(timestamp: Deadline, id: WindowID, x: Int, y: Int)
 
     /** Window has been resized to w x h */
-    case Resized(id: WindowID, w: Int, h: Int)
+    case Resized(timestamp: Deadline, id: WindowID, w: Int, h: Int)
 
     /** The pixel size of the window has changed to pw x ph */
-    case PixelSizeChanged(id: WindowID, pw: Int, ph: Int)
+    case PixelSizeChanged(timestamp: Deadline, id: WindowID, pw: Int, ph: Int)
 
     /** The pixel size of a Metal view associated with the window has changed */
-    case MetalViewResized(id: WindowID)
+    case MetalViewResized(timestamp: Deadline, id: WindowID)
 
     /** Window has been minimized */
-    case Minimized(id: WindowID)
+    case Minimized(timestamp: Deadline, id: WindowID)
 
     /** Window has been maximized */
-    case Maximized(id: WindowID)
+    case Maximized(timestamp: Deadline, id: WindowID)
 
     /** Window has been restored to normal size and position */
-    case Restored(id: WindowID)
+    case Restored(timestamp: Deadline, id: WindowID)
 
     /** Window has gained mouse focus */
-    case MouseEnter(id: WindowID)
+    case MouseEnter(timestamp: Deadline, id: WindowID)
 
     /** Window has lost mouse focus */
-    case MouseLeave(id: WindowID)
+    case MouseLeave(timestamp: Deadline, id: WindowID)
 
     /** Window has gained keyboard focus */
-    case FocusGained(id: WindowID)
+    case FocusGained(timestamp: Deadline, id: WindowID)
 
     /** Window has lost keyboard focus */
-    case FocusLost(id: WindowID)
+    case FocusLost(timestamp: Deadline, id: WindowID)
 
     /** The window manager requests that the window be closed */
-    case CloseRequested(id: WindowID)
+    case CloseRequested(timestamp: Deadline, id: WindowID)
 
     /** Window had a hit test that wasn't SDL_HITTEST_NORMAL */
-    case HitTest(id: WindowID)
+    case HitTest(timestamp: Deadline, id: WindowID)
 
     /** The ICC profile of the window's display has changed */
-    case ICCProfChanged(id: WindowID)
+    case ICCProfChanged(timestamp: Deadline, id: WindowID)
 
     /** Window has been moved to display `display` */
-    case DisplayChanged(id: WindowID, display: DisplayID)
+    case DisplayChanged(timestamp: Deadline, id: WindowID, display: DisplayID)
 
     /** Window display scale has been changed */
-    case DisplayScaleChanged(id: WindowID)
+    case DisplayScaleChanged(timestamp: Deadline, id: WindowID)
 
     /** The window safe area has been changed */
-    case SafeAreaChanged(id: WindowID)
+    case SafeAreaChanged(timestamp: Deadline, id: WindowID)
 
     /** The window has been occluded */
-    case Occluded(id: WindowID)
+    case Occluded(timestamp: Deadline, id: WindowID)
 
     /** The window has entered fullscreen mode */
-    case EnterFullscreen(id: WindowID)
+    case EnterFullscreen(timestamp: Deadline, id: WindowID)
 
     /** The window has left fullscreen mode */
-    case LeaveFullscreen(id: WindowID)
+    case LeaveFullscreen(timestamp: Deadline, id: WindowID)
 
     /** The window with the associated ID is being or has been destroyed. If this
       * message is being handled in an event watcher, the window handle is still
@@ -118,10 +119,10 @@ object Event:
       * window. Otherwise, the handle has already been destroyed and all resources
       * associated with it are invalid
       */
-    case Destroyed(id: WindowID)
+    case Destroyed(timestamp: Deadline, id: WindowID)
 
     /** Window HDR properties have changed */
-    case HDRStateChanged(id: WindowID)
+    case HDRStateChanged(timestamp: Deadline, id: WindowID)
 
   enum Key extends Event:
     def windowID: WindowID
@@ -132,53 +133,53 @@ object Event:
     def down: Boolean
     def repeat: Boolean
 
-    case Down(windowID: WindowID, which: KeyboardID, scancode: Scancode, key: Keycode, mod: Set[Keymod], down: Boolean, repeat: Boolean)
-    case Up(windowID: WindowID, which: KeyboardID, scancode: Scancode, key: Keycode, mod: Set[Keymod], down: Boolean, repeat: Boolean)
+    case Down(timestamp: Deadline, windowID: WindowID, which: KeyboardID, scancode: Scancode, key: Keycode, mod: Set[Keymod], down: Boolean, repeat: Boolean)
+    case Up(timestamp: Deadline, windowID: WindowID, which: KeyboardID, scancode: Scancode, key: Keycode, mod: Set[Keymod], down: Boolean, repeat: Boolean)
 
-  case class TextEditing(windowID: WindowID, text: String, start: Int, length: Int) extends Event
-  case class TextInput(windowID: WindowID, text: String) extends Event
-  case object KeymapChanged extends Event
-  case class KeyboardAdded(which: KeyboardID) extends Event
-  case class KeyboardRemoved(which: KeyboardID) extends Event
-  case class TextEditingCandidates(windowID: WindowID, candidates: Seq[String], selectedCandidate: Option[Int], horizontal: Boolean) extends Event
+  case class TextEditing(timestamp: Deadline, windowID: WindowID, text: String, start: Int, length: Int) extends Event
+  case class TextInput(timestamp: Deadline, windowID: WindowID, text: String) extends Event
+  case class KeymapChanged(timestamp: Deadline) extends Event
+  case class KeyboardAdded(timestamp: Deadline, which: KeyboardID) extends Event
+  case class KeyboardRemoved(timestamp: Deadline, which: KeyboardID) extends Event
+  case class TextEditingCandidates(timestamp: Deadline, windowID: WindowID, candidates: Seq[String], selectedCandidate: Option[Int], horizontal: Boolean) extends Event
 
   enum Mouse extends Event:
     def which: MouseID
 
-    case ButtonDown(windowID: WindowID, which: MouseID, button: Byte, down: Boolean, clicks: Byte, x: Float, y: Float)
-    case ButtonUp(windowID: WindowID, which: MouseID, button: Byte, down: Boolean, clicks: Byte, x: Float, y: Float)
-    case Motion(which: MouseID, state: Set[mouse.Button], x: Float, y: Float, xrel: Float, yrel: Float)
-    case Added(which: MouseID)
-    case Removed(which: MouseID)
+    case ButtonDown(timestamp: Deadline, windowID: WindowID, which: MouseID, button: Byte, down: Boolean, clicks: Byte, x: Float, y: Float)
+    case ButtonUp(timestamp: Deadline, windowID: WindowID, which: MouseID, button: Byte, down: Boolean, clicks: Byte, x: Float, y: Float)
+    case Motion(timestamp: Deadline, which: MouseID, state: Set[mouse.Button], x: Float, y: Float, xrel: Float, yrel: Float)
+    case Added(timestamp: Deadline, which: MouseID)
+    case Removed(timestamp: Deadline, which: MouseID)
 
   enum Joystick extends Event:
     def which: JoystickID
 
-    case ButtonDown(which: JoystickID, button: Byte, down: Boolean)
-    case ButtonUp(which: JoystickID, button: Byte, down: Boolean)
-    case AxisMotion(which: JoystickID, axis: Byte, value: Int)
-    case BallMotion(which: JoystickID, ball: Byte, xrel: Int, yrel: Int)
-    case HatMotion(which: JoystickID, hat: Byte, value: HatPosition)
-    case Added(which: JoystickID)
-    case Removed(which: JoystickID)
-    case BatteryUpdated(which: JoystickID, state: PowerState, percent: Int)
-    case UpdateComplete(which: JoystickID)
+    case ButtonDown(timestamp: Deadline, which: JoystickID, button: Byte, down: Boolean)
+    case ButtonUp(timestamp: Deadline, which: JoystickID, button: Byte, down: Boolean)
+    case AxisMotion(timestamp: Deadline, which: JoystickID, axis: Byte, value: Int)
+    case BallMotion(timestamp: Deadline, which: JoystickID, ball: Byte, xrel: Int, yrel: Int)
+    case HatMotion(timestamp: Deadline, which: JoystickID, hat: Byte, value: HatPosition)
+    case Added(timestamp: Deadline, which: JoystickID)
+    case Removed(timestamp: Deadline, which: JoystickID)
+    case BatteryUpdated(timestamp: Deadline, which: JoystickID, state: PowerState, percent: Int)
+    case UpdateComplete(timestamp: Deadline, which: JoystickID)
 
   enum Gamepad extends Event:
     def which: JoystickID
 
-    case ButtonDown(which: JoystickID, button: gamepad.Gamepad.Button, down: Boolean)
-    case ButtonUp(which: JoystickID, button: gamepad.Gamepad.Button, down: Boolean)
-    case AxisMotion(which: JoystickID, axis: gamepad.Gamepad.Axis, value: Int)
-    case Added(which: JoystickID)
-    case Removed(which: JoystickID)
-    case Remapped(which: JoystickID)
-    case TouchpadDown(which: JoystickID, touchpad: Int, finger: Int, x: Float, y: Float, pressure: Float)
-    case TouchpadMotion(which: JoystickID, touchpad: Int, finger: Int, x: Float, y: Float, pressure: Float)
-    case TouchpadUp(which: JoystickID, touchpad: Int, finger: Int, x: Float, y: Float, pressure: Float)
-    case SensorUpdate(which: JoystickID, data: SensorData, sensorTimestamp: Long)
-    case UpdateComplete(which: JoystickID)
-    case SteamHandleUpdated(which: JoystickID)
+    case ButtonDown(timestamp: Deadline, which: JoystickID, button: gamepad.Gamepad.Button, down: Boolean)
+    case ButtonUp(timestamp: Deadline, which: JoystickID, button: gamepad.Gamepad.Button, down: Boolean)
+    case AxisMotion(timestamp: Deadline, which: JoystickID, axis: gamepad.Gamepad.Axis, value: Int)
+    case Added(timestamp: Deadline, which: JoystickID)
+    case Removed(timestamp: Deadline, which: JoystickID)
+    case Remapped(timestamp: Deadline, which: JoystickID)
+    case TouchpadDown(timestamp: Deadline, which: JoystickID, touchpad: Int, finger: Int, x: Float, y: Float, pressure: Float)
+    case TouchpadMotion(timestamp: Deadline, which: JoystickID, touchpad: Int, finger: Int, x: Float, y: Float, pressure: Float)
+    case TouchpadUp(timestamp: Deadline, which: JoystickID, touchpad: Int, finger: Int, x: Float, y: Float, pressure: Float)
+    case SensorUpdate(timestamp: Deadline, which: JoystickID, data: SensorData, sensorTimestamp: Long)
+    case UpdateComplete(timestamp: Deadline, which: JoystickID)
+    case SteamHandleUpdated(timestamp: Deadline, which: JoystickID)
 
   enum Finger extends Event:
     def touchID: touch.TouchID
@@ -190,52 +191,52 @@ object Event:
     def pressure: Float
     def windowID: WindowID
 
-    case Down(touchID: touch.TouchID, fingerID: touch.FingerID, x: Float, y: Float, dx: Float, dy: Float, pressure: Float, windowID: WindowID)
-    case Up(touchID: touch.TouchID, fingerID: touch.FingerID, x: Float, y: Float, dx: Float, dy: Float, pressure: Float, windowID: WindowID)
-    case Motion(touchID: touch.TouchID, fingerID: touch.FingerID, x: Float, y: Float, dx: Float, dy: Float, pressure: Float, windowID: WindowID)
-    case Canceled(touchID: touch.TouchID, fingerID: touch.FingerID, x: Float, y: Float, dx: Float, dy: Float, pressure: Float, windowID: WindowID)
+    case Down(timestamp: Deadline, touchID: touch.TouchID, fingerID: touch.FingerID, x: Float, y: Float, dx: Float, dy: Float, pressure: Float, windowID: WindowID)
+    case Up(timestamp: Deadline, touchID: touch.TouchID, fingerID: touch.FingerID, x: Float, y: Float, dx: Float, dy: Float, pressure: Float, windowID: WindowID)
+    case Motion(timestamp: Deadline, touchID: touch.TouchID, fingerID: touch.FingerID, x: Float, y: Float, dx: Float, dy: Float, pressure: Float, windowID: WindowID)
+    case Canceled(timestamp: Deadline, touchID: touch.TouchID, fingerID: touch.FingerID, x: Float, y: Float, dx: Float, dy: Float, pressure: Float, windowID: WindowID)
 
   /** An event which lets you handle the clipboard
     * @param owner are we owning the clipboard? (internal update)
     * @param mimeTypes current mime types, a mime type is the kind of data contained in the clipboard. Examples of MIME-types are text/javascript or image/png.
     */
-  case class ClipboardUpdate(owner: Boolean, mimeTypes: String*) extends Event
+  case class ClipboardUpdate(timestamp: Deadline, owner: Boolean, mimeTypes: String*) extends Event
 
   enum Drop extends Event:
     def windowID: WindowID
     def source: Option[String]
 
-    case File(windowID: WindowID, x: Float, y: Float, source: Option[String], filename: String)
-    case Text(windowID: WindowID, x: Float, y: Float, source: Option[String], text: String)
-    case Begin(windowID: WindowID, source: Option[String])
-    case Complete(windowID: WindowID, x: Float, y: Float, source: Option[String])
-    case Position(windowID: WindowID, x: Float, y: Float, source: Option[String])
+    case File(timestamp: Deadline, windowID: WindowID, x: Float, y: Float, source: Option[String], filename: String)
+    case Text(timestamp: Deadline, windowID: WindowID, x: Float, y: Float, source: Option[String], text: String)
+    case Begin(timestamp: Deadline, windowID: WindowID, source: Option[String])
+    case Complete(timestamp: Deadline, windowID: WindowID, x: Float, y: Float, source: Option[String])
+    case Position(timestamp: Deadline, windowID: WindowID, x: Float, y: Float, source: Option[String])
 
   enum Pen extends Event:
     def windowID: WindowID
     def which: PenID
     
-    case ProximityIn(windowID: WindowID, which: PenID)
-    case ProximityOut(windowID: WindowID, which: PenID)
-    case Down(windowID: WindowID, which: PenID, penState: Set[PenInputFlags], x: Float, y: Float, eraser: Boolean, down: Boolean)
-    case Up(windowID: WindowID, which: PenID, penState: Set[PenInputFlags], x: Float, y: Float, eraser: Boolean, down: Boolean)
-    case ButtonDown(windowID: WindowID, which: PenID, penState: Set[PenInputFlags])
-    case ButtonUp(windowID: WindowID, which: PenID, penState: Set[PenInputFlags])
-    case Motion(windowID: WindowID, which: PenID, penState: Set[PenInputFlags], x: Float, y: Float)
-    case Axis(windowID: WindowID, which: PenID, penState: Set[PenInputFlags], x: Float, y: Float, axis: PenAxis, value: Float)
+    case ProximityIn(timestamp: Deadline, windowID: WindowID, which: PenID)
+    case ProximityOut(timestamp: Deadline, windowID: WindowID, which: PenID)
+    case Down(timestamp: Deadline, windowID: WindowID, which: PenID, penState: Set[PenInputFlags], x: Float, y: Float, eraser: Boolean, down: Boolean)
+    case Up(timestamp: Deadline, windowID: WindowID, which: PenID, penState: Set[PenInputFlags], x: Float, y: Float, eraser: Boolean, down: Boolean)
+    case ButtonDown(timestamp: Deadline, windowID: WindowID, which: PenID, penState: Set[PenInputFlags])
+    case ButtonUp(timestamp: Deadline, windowID: WindowID, which: PenID, penState: Set[PenInputFlags])
+    case Motion(timestamp: Deadline, windowID: WindowID, which: PenID, penState: Set[PenInputFlags], x: Float, y: Float)
+    case Axis(timestamp: Deadline, windowID: WindowID, which: PenID, penState: Set[PenInputFlags], x: Float, y: Float, axis: PenAxis, value: Float)
 
   enum CameraDevice extends Event:
     def which: CameraID
     
-    case Added(which: CameraID)
-    case Removed(which: CameraID)
-    case Approved(which: CameraID)
-    case Denied(which: CameraID)
+    case Added(timestamp: Deadline, which: CameraID)
+    case Removed(timestamp: Deadline, which: CameraID)
+    case Approved(timestamp: Deadline, which: CameraID)
+    case Denied(timestamp: Deadline, which: CameraID)
 
   enum Render extends Event:
     def windowID: WindowID
 
-    case TargetsReset(windowID: WindowID)
-    case DeviceReset(windowID: WindowID)
-    case DeviceLost(windowID: WindowID)
+    case TargetsReset(timestamp: Deadline, windowID: WindowID)
+    case DeviceReset(timestamp: Deadline, windowID: WindowID)
+    case DeviceLost(timestamp: Deadline, windowID: WindowID)
 end Event
