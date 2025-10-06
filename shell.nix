99894eb64640da8e49e82f29c5f7e79911ab225b
Hampus Edén <hampus.eden@gmail.com>
2025-10-06 16:55:07 +0200

    add runtime dynamically linked libraries to shell.nix

--- shell.nix
+++ shell.nix
@@ -2,11 +2,25 @@
     fetchTarball "https://github.com/NixOS/nixpkgs/archive/647e5c14cbd5067f44ac86b74f014962df460840.tar.gz"
   ) {}
 }:
-pkgs.mkShellNoCC {
-  packages = with pkgs; [
-    jdk21
-    scala-next
-    scala-cli
-    mill
+let
+  runtimeLibs = with pkgs; lib.makeLibraryPath [
+    # xorg.libX11
+    # xorg.libXcursor
+    # xorg.libXext
+    # xorg.libXrandr
+    # xorg.libXxf86vm
+    wayland
+    libxkbcommon
   ];
-}
+in
+  pkgs.mkShell {
+    shellHook = ''
+      export LD_LIBRARY_PATH=${runtimeLibs}:$LD_LIBRARY_PATH
+    '';
+    packages = with pkgs; [
+      jdk21
+      scala-next
+      scala-cli
+      mill
+    ];
+  }
