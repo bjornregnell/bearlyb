d6c81caccbb2095af0c1bd0d45824ca7f0058218
Hampus Edén <hampus.eden@gmail.com>
2025-10-25 19:27:52 +0200

    update shell.nix to make it match 25.05 sdl3 package in nixpkgs

--- shell.nix
+++ shell.nix
@@ -23,6 +23,46 @@ let
   traySupport = true;
   waylandSupport = stdenv.hostPlatform.isLinux && !stdenv.hostPlatform.isAndroid;
   x11Support = !stdenv.hostPlatform.isAndroid && !stdenv.hostPlatform.isWindows;
+
+  dlopenBuildInputs = with pkgs;
+    lib.optionals stdenv.hostPlatform.isLinux [
+      libusb1
+    ]
+    ++ lib.optional (
+      stdenv.hostPlatform.isUnix && !stdenv.hostPlatform.isDarwin
+    ) libayatana-appindicator
+    ++ lib.optional alsaSupport alsa-lib
+    ++ lib.optional dbusSupport dbus
+    ++ lib.optionals drmSupport [
+      libdrm
+      libgbm
+    ]
+    ++ lib.optional jackSupport libjack2
+    ++ lib.optional libdecorSupport libdecor
+    ++ lib.optional libudevSupport systemdLibs
+    ++ lib.optional openglSupport libGL
+    ++ lib.optional pipewireSupport pipewire
+    ++ lib.optional pulseaudioSupport libpulseaudio
+    ++ lib.optional sndioSupport sndio
+    ++ lib.optionals waylandSupport [
+      libxkbcommon
+      wayland
+    ]
+    ++ lib.optionals x11Support [
+      xorg.libX11
+      xorg.libXScrnSaver
+      xorg.libXcursor
+      xorg.libXext
+      xorg.libXfixes
+      xorg.libXi
+      xorg.libXrandr
+    ]
+    ++ [
+      vulkan-headers
+      vulkan-loader
+    ]
+    ++ lib.optional (openglSupport && !stdenv.hostPlatform.isDarwin) libGL
+    ++ lib.optional x11Support xorg.libX11;
 in
   assert lib.assertMsg (
     waylandSupport -> openglSupport
@@ -32,65 +72,13 @@ in
   ) "SDL3 requires dbus support to enable ibus";
 
   pkgs.mkShell rec {
-    shellHook = ''
-      export LD_LIBRARY_PATH=${lib.makeLibraryPath (buildInputs ++ nativeBuildInputs)}:$LD_LIBRARY_PATH
-    '';
+    buildInputs = dlopenBuildInputs;
 
-    nativeBuildInputs = with pkgs; [] ++ lib.optional waylandSupport wayland-scanner;
-
-    buildInputs = with pkgs;
-      lib.optionals stdenv.hostPlatform.isLinux [
-        libusb1
-      ]
-      ++ lib.optional (
-        stdenv.hostPlatform.isUnix && !stdenv.hostPlatform.isDarwin && traySupport
-      ) libayatana-appindicator
-      ++ lib.optional alsaSupport alsa-lib
-      ++ lib.optional dbusSupport dbus
-      ++ lib.optionals drmSupport [
-        libdrm
-        libgbm
-      ]
-      ++ lib.optional jackSupport libjack2
-      ++ lib.optional libdecorSupport libdecor
-      ++ lib.optional libudevSupport systemdLibs
-      ++ lib.optional openglSupport libGL
-      ++ lib.optional pipewireSupport pipewire
-      ++ lib.optional pulseaudioSupport libpulseaudio
-      ++ lib.optional sndioSupport sndio
-      ++ lib.optionals waylandSupport [
-        libxkbcommon
-        wayland
-      ]
-      ++ lib.optionals x11Support [
-        xorg.libX11
-        xorg.libxcb
-        xorg.libXScrnSaver
-        xorg.libXcursor
-        xorg.libXext
-        xorg.libXfixes
-        xorg.libXi
-        xorg.libXrandr
-      ]
-      ++ [
-        vulkan-headers
-        vulkan-loader
-      ]
-      ++ lib.optional (openglSupport && !stdenv.hostPlatform.isDarwin) libGL
-      ++ lib.optional x11Support xorg.libX11
-      ++ lib.optionals stdenv.hostPlatform.isDarwin [
-        # error: 'MTLPixelFormatASTC_4x4_LDR' is unavailable: not available on macOS
-        (darwinMinVersionHook "11.0")
-
-        apple-sdk_11
-      ]
-      ++ lib.optionals ibusSupport [
-        # sdl3 only uses some constants of the ibus headers
-        # it never actually loads the library
-        # thus, it also does not have to care about gtk integration,
-        # so using ibusMinimal avoids an unnecessarily large closure here.
-        ibusMinimal
-      ];
+    # Many dependencies are not directly linked to, but dlopen()'d at runtime. Adding them to the RPATH
+    # helps them be found
+    LD_LIBRARY_PATH = lib.optionalString (
+      stdenv.hostPlatform.hasSharedLibraries && stdenv.hostPlatform.extensions.sharedLibrary == ".so"
+    ) "-rpath ${lib.makeLibraryPath (dlopenBuildInputs)}";
 
     packages = with pkgs; [
       jdk21
